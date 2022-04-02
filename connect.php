1c429d507f1d7b001b2c4db0bf05499b9ba164d6
Monirul Islam Suraj <98105557+SurajChyy@users.noreply.github.com>
2022-04-02 12:06:22 +0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,44 @@
+<!-- Implement a simple calculator web application that takes two numbers and an operator ( ,-,/,* and %)from an HTML page and returns the result page with the operation performed on the operands. -->
+
+<?php
+$num1=$_POST["fnum"];
+$num2=$_POST["snum"];
+$op=$_POST["op"];
+if($num1=="" or $num2=="")
+{
+    echo "Please enter two numbers...";
+}
+else
+{
+    echo "First Number is : ".$num1."<br/>"; 
+    echo "Second Number is : ".$num2."<br/>";
+    echo "Operator is : ".$op."<br/>";
+    if($op=="+")
+    {
+        $res=$num1+$num2;
+        echo "Additon is : ".$res;
+    }
+    if($op=="-")
+    {
+        $res=$num1-$num2;
+        echo "Subtraction is : ".$res;
+    }
+    if($op=="*")
+    {
+        $res=$num1*$num2;
+        echo "Multiplication is : ".$res;
+    }
+    if($op=="/")
+    {
+        $res=$num1/$num2;
+        echo "Division is : ".$res;
+    }
+    if($op=="%")
+    {
+        $res=$num1%$num2;
+        echo "Modulus is : ".$res;
+    }
+}
+
+?>
+
